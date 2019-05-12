80bd92785f5e6159c3b07a84ff1b44cd82226f31
Michael Maggs <michaelmaggs@gmail.com>
2019-05-12 23:48:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
+def prime?(num)
+    (2...num).each do |number|
+      if num < 2
+        return false 
+      elsif num % number == 0 
+        return false 
+      else 
+        return true 
+    end
+  end
+end
+    
+    
+
+
 # Add  code here!
